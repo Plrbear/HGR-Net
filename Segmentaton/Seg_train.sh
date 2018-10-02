6e70e0e6604e02ecc76d2c6fb4ed29df8243f80e
Amirhossein Dadashzadeh <31938815+Plrbear@users.noreply.github.com>
2018-10-03 00:19:59 +0330

    Update Seg_train.sh

--- Segmentaton/Seg_train.sh
+++ Segmentaton/Seg_train.sh
@@ -5,7 +5,7 @@ python train.py --batch_size 2 --epochs 200 --lr 0.001 \
                 --ad2 'train' \
                 --ad3 'validation' \
                 --ad5 'masks' \
-		--img_format '*png' --chekp 'hi' \
+		--img_format '*png' --chekp 'Seg_weight' \
                 --row 160 --col 160 --ch 3  
 
                  
