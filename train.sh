2b98491cde468a7b4081c04db971427061f010a5
Amirhossein Dadashzadeh <31938815+Plrbear@users.noreply.github.com>
2018-10-04 02:15:30 +0330

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
 python train.py --batch_size 2 --epochs 200 --lr 0.001 \
-                --train_ad '/home/amir/Downloads/HGR-Net-master (2)/ntrain' \
-                --validation_ad '/home/amir/Downloads/HGR-Net-master (2)/ntrain' \
+                --train_ad '/home/amir/hgr/train' \
+                --validation_ad '/home/amir/hgr/validation' \
                 --chekp 'chpoint' \
-                --row 480 --col 320 --ch 3  --num_val 33 --num_img 33
-
+                --row 480 --col 320 --ch 3  --num_val 33 --num_img 33 --num_class 10
